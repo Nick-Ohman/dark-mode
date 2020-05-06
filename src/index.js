4c6cfaa246c1b2364f3e4c24a58933992cdcadd2
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-05-06 18:16:07 -0500

    MVP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import NewPage from "./components/NewPage"
 
 import "./styles.scss";
 
@@ -15,16 +17,31 @@ const App = () => {
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
+
       .then(res => setCoinData(res.data))
+
       .catch(err => console.log(err));
   }, []);
   return (
+
     <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
+      
+        <Route exact path='/'>
+          <Navbar />
+        </Route>
+        
+        <Route exact path='/'>
+          <Charts coinData={coinData} />
+        </Route>
+        
+        
+      
+
+
     </div>
+
   );
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Router> <App /> </Router>, rootElement);
